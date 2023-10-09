cba606797f10cf7758ebb1316b11cf3f1210543e
BertanTurgut <bertanturgut04@gmail.com>
2023-10-10 02:03:21 +0300

    Growing Roots

--- service_methods.py
+++ service_methods.py
@@ -3,5 +3,5 @@ from spotipy.oauth2 import SpotifyOAuth
 
 def createPlaylist(spotipy_client: spotipy, name, public=True, collaborative=False, description=''):
     user_dictionary = spotipy_client.current_user()
-    user_id = user_id = user_dictionary['id']
+    user_id = user_dictionary['id']
     spotipy_client.user_playlist_create(user_id, name, public, collaborative, description)
