4dd9de1e2b0b743806b6e31e179bba0b5dc4a0cf
BertanTurgut <bertanturgut04@gmail.com>
2023-10-10 01:41:04 +0300

    Growing Roots

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ import secret
 # Suitable scope for the purpose
 scope = 'playlist-modify-public'
 # Authentication and creating Spotipy object
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secret.client_id, client_secret=secret.client_secret, redirect_uri=secret.redirect_url, scope=scope))
+spotipy_object = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secret.client_id, client_secret=secret.client_secret, redirect_uri=secret.redirect_url, scope=scope))
+
+# Collect current user's dictionary
+user_dictionary = spotipy_object.current_user()
+user_id = user_dictionary['id']
+
 
